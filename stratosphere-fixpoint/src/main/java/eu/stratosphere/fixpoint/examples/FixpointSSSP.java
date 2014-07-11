daf44e47e31423727aae1caf76750fb41ad68733
vasia <vasilikikalavri@gmail.com>
2014-07-11 13:57:33 +0200

    simple PageRank and SSSP fixpoint examples

--- stratosphere-fixpoint/src/main/java/eu/stratosphere/fixpoint/examples/FixpointSSSP.java
+++ stratosphere-fixpoint/src/main/java/eu/stratosphere/fixpoint/examples/FixpointSSSP.java
@@ -4,16 +4,17 @@ import eu.stratosphere.api.common.ProgramDescription;
 import eu.stratosphere.api.java.DataSet;
 import eu.stratosphere.api.java.ExecutionEnvironment;
 import eu.stratosphere.api.java.aggregation.Aggregations;
+import eu.stratosphere.api.java.functions.FlatMapFunction;
 import eu.stratosphere.api.java.tuple.Tuple2;
 import eu.stratosphere.api.java.tuple.Tuple3;
 import eu.stratosphere.api.java.tuple.Tuple4;
 import eu.stratosphere.fixpoint.api.FixedPointIteration;
 import eu.stratosphere.fixpoint.api.StepFunction;
+import eu.stratosphere.util.Collector;
 
-public class FixpointConnectedComponents implements ProgramDescription {
+public class FixpointSSSP implements ProgramDescription {
 
 	public static void main(String... args) throws Exception {
-		
 		if (args.length < 4) {
 			System.err.println("Parameters: <vertices-path> <edges-path> <result-path> <max_iterations>");
 			return;
@@ -27,33 +28,53 @@ public class FixpointConnectedComponents implements ProgramDescription {
 				Long.class); 
 		
 		int maxIterations = Integer.parseInt(args[3]);
-	
+		
 		DataSet<Tuple2<Long, Long>> result = vertices.runOperation(FixedPointIteration.withWeightedDependencies(edges, 
-				new MinId(), maxIterations));
+				new ShortestPath(), maxIterations));
 
 		result.print();
-		env.execute("Fixed Point Connected Components");
+		env.execute("Fixed Point SSSP");
 		
 	}
-	
-	@SuppressWarnings("serial")
-	public static final class MinId extends StepFunction<Long, Long, Long> {
+
+	public static final class ShortestPath extends StepFunction<Long, Long, Long> {
+
+		private static final long serialVersionUID = 1L;
 
 		@Override
 		public DataSet<Tuple2<Long, Long>> updateState(
 				DataSet<Tuple4<Long, Long, Long, Long>> inNeighbors) {
 			
-			DataSet<Tuple3<Long, Long, Long>> groupedNeighbors = inNeighbors.groupBy(0).aggregate(Aggregations.MIN, 2)
-																	.project(0, 2, 3)
-																	.types(Long.class, Long.class, Long.class);
-			return groupedNeighbors.project(0, 1).types(Long.class, Long.class);
+			DataSet<Tuple2<Long, Long>> updatedDistances = inNeighbors.flatMap(new CandidateDistances())
+															.groupBy(0).aggregate(Aggregations.MIN, 1)
+															.project(0, 1).types(Long.class, Long.class);
+			return updatedDistances;
 		}
 		
+	}
+	
+	public static final class CandidateDistances extends FlatMapFunction<Tuple4<Long, Long, Long, Long>,
+			Tuple2<Long, Long>> {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void flatMap(Tuple4<Long, Long, Long, Long> value,
+				Collector<Tuple2<Long, Long>> out) throws Exception {
+			if (value.f0 == value.f1) {
+				// own value
+				out.collect(new Tuple2<Long, Long>(value.f0, value.f2));
+			}
+			else {
+				out.collect(new Tuple2<Long, Long>(value.f0, value.f2 + 1));
+			}
+			
+		}
+	
 	}
 	
 	@Override
 	public String getDescription() {
 		return "Parameters: <vertices-path> <edges-path> <result-path> <max-number-of-iterations>";
 	}
-	
 }
---
+++ stratosphere-fixpoint/src/main/java/eu/stratosphere/fixpoint/examples/FixpointSimpleRageRank.java
@@ -0,0 +1,74 @@
+package eu.stratosphere.fixpoint.examples;
+
+import eu.stratosphere.api.common.ProgramDescription;
+import eu.stratosphere.api.java.DataSet;
+import eu.stratosphere.api.java.ExecutionEnvironment;
+import eu.stratosphere.api.java.aggregation.Aggregations;
+import eu.stratosphere.api.java.functions.MapFunction;
+import eu.stratosphere.api.java.tuple.Tuple2;
+import eu.stratosphere.api.java.tuple.Tuple3;
+import eu.stratosphere.api.java.tuple.Tuple4;
+import eu.stratosphere.fixpoint.api.FixedPointIteration;
+import eu.stratosphere.fixpoint.api.StepFunction;
+
+public class FixpointSimpleRageRank implements ProgramDescription {
+
+	public static void main(String... args) throws Exception {
+		
+		if (args.length < 4) {
+			System.err.println("Parameters: <vertices-path> <edges-path> <result-path> <max_iterations>");
+			return;
+		}
+		
+		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		
+		DataSet<Tuple2<Long, Double>> vertices = env.readCsvFile(args[0]).fieldDelimiter(' ').types(Long.class, Double.class);
+		
+		DataSet<Tuple3<Long, Long, Long>> edges = env.readCsvFile(args[1]).fieldDelimiter(' ').types(Long.class, Long.class, 
+				Long.class); 
+		
+		int maxIterations = Integer.parseInt(args[3]);
+		
+		DataSet<Tuple2<Long, Double>> result = vertices.runOperation(FixedPointIteration.withWeightedDependencies(edges, 
+				new UpdateRanks(), maxIterations));
+
+		result.print();
+		env.execute("Fixed Point Simple PageRank");
+	}
+	
+	@SuppressWarnings("serial")
+	public static final class UpdateRanks extends StepFunction<Long, Double, Long> {
+
+		@Override
+		public DataSet<Tuple2<Long, Double>> updateState(
+				DataSet<Tuple4<Long, Long, Double, Long>> inNeighbors) {
+			
+			DataSet<Tuple2<Long, Double>> newRanks = inNeighbors.map(new PartialRankMapper())
+														.groupBy(0).aggregate(Aggregations.SUM, 1)
+														.project(0, 1).types(Long.class, Double.class);
+			return newRanks;
+		}
+		
+	}
+	
+	public static final class PartialRankMapper extends MapFunction<Tuple4<Long, Long, Double, Long>, 
+		Tuple2<Long, Double>> {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public Tuple2<Long, Double> map(Tuple4<Long, Long, Double, Long> value)
+				throws Exception {
+			double partialRank = value.f2 / (double) value.f3;
+			
+			return new Tuple2<Long, Double>(value.f0, partialRank);
+		}
+		
+	}
+	
+	@Override
+	public String getDescription() {
+		return "Parameters: <vertices-path> <edges-path> <result-path> <max-number-of-iterations>";
+	}
+
+}
